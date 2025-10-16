02590ef61dd91ad0852005cb4cecddc7e5013bdc
pca <epsl0630@gmail.com>
2025-10-16 18:33:30 +0900

    ApprovalController, ApprovalRepository 작성 ScheduleController, ScheduleRepository 작성 AttachmentController 작성
    추후 수정필요

--- Mocha04/src/main/java/mocha/ezen/com/approval/ApprovalController.java
+++ Mocha04/src/main/java/mocha/ezen/com/approval/ApprovalController.java
@@ -1,5 +1,211 @@
 package mocha.ezen.com.approval;
 
-public class ApprovalController {
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import mocha.ezen.com.attachment.AttachmentDTO;
+import mocha.ezen.com.schedule.ScheduleDTO;
+import mocha.ezen.com.schedule.ScheduleRepository;
+
+@Controller
+@RequestMapping("/approval")
+public class ApprovalController 
+{
+	//업로드 경로
+	private static final String uploadPath = "C:\\Users\\MYCOM\\git\\repository4\\Mocha04\\upload";
+	
+		@Autowired
+		ApprovalRepository  approvalRepository;
+		
+		@RequestMapping(value = "/ApprovalList")
+		public String ApprovalList()
+		{
+			//페이징, 결재구분 
+			
+			return "approval/approvalList";
+		}
+		
+		@RequestMapping(value = "/DraftList")
+		public String DraftList()
+		{
+			//페이징
+			
+			return "approval/draftList";
+		}
+	
+		@RequestMapping(value = "/AddApprovalList")
+		public String AddApprovalList()
+		{
+			//페이징
+			
+			return "approval/addApprovalList";
+		}
+		
+		@RequestMapping(value = "/DraftWrite")
+		public String DraftWrite()
+		{
+			return "draftWrite";
+		}	
+		
+		@RequestMapping(value = "/DraftWriteOK", method = RequestMethod.POST)
+		public String DraftWriteOK(ApprovalDTO dto,
+				@RequestParam("attach")MultipartFile file,
+				HttpServletRequest request, AttachmentDTO adto) throws IllegalStateException, IOException
+		{	
+			if(file != null)
+			{
+				//첨부파일 업로드 되었음.
+				
+				//업로드된 원본 파일 이름 가져오기
+				String originalFileName = file.getOriginalFilename();
+				System.out.println("originalFileName:" + originalFileName);
+				
+				//파일 이름이 중복되지 않도록 파일 이름 변경 : 서버에 저장할 이름
+				// UUID 클래스 사용
+				UUID uuid = UUID.randomUUID();
+				String savedFileName = uuid.toString();
+				System.out.println("savedFileName:" + savedFileName);
+				
+				//첨부파일 객체 생성
+				java.io.File newFile = new File(uploadPath + "\\" + savedFileName);
+				
+				//실제 저장 디렉토리로 전송		
+				file.transferTo(newFile);
+				
+				adto.setFname(originalFileName); //원본파일명
+				adto.setPname(savedFileName);    //저장된 파일명
+			}
+			
+			approvalRepository.Insert(dto);
+			
+			return "redirect:/approvalView?no=" + dto.getApproval_no();
+		}	
+		
+		@RequestMapping(value = "/ApprovalModify")
+		public String ApprovalModify(@RequestParam(required = true) String no,
+				Model model)
+		{
+			//게시물 정보 조회
+			ApprovalDTO dto = approvalRepository.Read(no, false);
+
+			model.addAttribute("item",dto);
+			
+			return "approvalmodify";
+		}
+	
+		@RequestMapping(value = "/ApprovalModifyOK", method = RequestMethod.POST)
+		public String ApprovalModifyOK(ApprovalDTO dto,
+				@RequestParam("attach")MultipartFile file,
+				HttpServletRequest request, AttachmentDTO adto) throws IllegalStateException, IOException
+		{	
+			if(file != null)
+			{
+				//첨부파일 업로드 되었음.
+				
+				//업로드된 원본 파일 이름 가져오기
+				String originalFileName = file.getOriginalFilename();
+				System.out.println("originalFileName:" + originalFileName);
+				
+				//파일 이름이 중복되지 않도록 파일 이름 변경 : 서버에 저장할 이름
+				// UUID 클래스 사용
+				UUID uuid = UUID.randomUUID();
+				String savedFileName = uuid.toString();
+				System.out.println("savedFileName:" + savedFileName);
+				
+				//첨부파일 객체 생성
+				java.io.File newFile = new File(uploadPath + "\\" + savedFileName);
+				
+				//실제 저장 디렉토리로 전송		
+				file.transferTo(newFile);
+				
+				adto.setFname(originalFileName); //원본파일명
+				adto.setPname(savedFileName);    //저장된 파일명
+			}
+			
+			approvalRepository.Update(dto);
+			
+			return "redirect:/approvalView?no=" + dto.getApproval_no();
+		}
+		
+		@RequestMapping(value = "/ReDraftWrite")
+		public String ReDraftWrite()
+		{
+			return "reDraftWrite";
+		}	
+		
+		@RequestMapping(value = "/ReDraftWriteOK", method = RequestMethod.POST)
+		public String ReDraftWriteOK(ApprovalDTO dto,
+				@RequestParam("attach")MultipartFile file,
+				HttpServletRequest request, AttachmentDTO adto) throws IllegalStateException, IOException
+		{	
+			if(file != null)
+			{
+				//첨부파일 업로드 되었음.
+				
+				//업로드된 원본 파일 이름 가져오기
+				String originalFileName = file.getOriginalFilename();
+				System.out.println("originalFileName:" + originalFileName);
+				
+				//파일 이름이 중복되지 않도록 파일 이름 변경 : 서버에 저장할 이름
+				// UUID 클래스 사용
+				UUID uuid = UUID.randomUUID();
+				String savedFileName = uuid.toString();
+				System.out.println("savedFileName:" + savedFileName);
+				
+				//첨부파일 객체 생성
+				java.io.File newFile = new File(uploadPath + "\\" + savedFileName);
+				
+				//실제 저장 디렉토리로 전송		
+				file.transferTo(newFile);
+				
+				adto.setFname(originalFileName); //원본파일명
+				adto.setPname(savedFileName);    //저장된 파일명
+			}
+			
+			approvalRepository.Insert(dto);
+			
+			return "redirect:/approvalView?no=" + dto.getApproval_no();
+		}	
+		
+		@RequestMapping(value = "/ApprovalView", method = RequestMethod.GET)
+		public String ApprovalView(@RequestParam(required = true) String no,
+				Model model)
+		{
+			//게시물 정보 조회
+			ApprovalDTO dto = approvalRepository.Read(no, true);
+			
+			//결재선 목록을 조회
+			List<ApproverDTO> approver = approverRepository.GetList(no);
+			
+			model.addAttribute("item",dto);
+			model.addAttribute("reply",approver);
+			
+			return "approvalView";
+		}
+		
+	
+		@RequestMapping(value = "/ApprovalDelete.do")
+		public String ApprovalDelete(@RequestParam(required = true)String no)
+		{
+			ApprovalRepository.Delete(no);
+			return "redirect:/DraftList";
+		}
+		
+		
+	
 }
--- Mocha04/src/main/java/mocha/ezen/com/attachment/AttachmentController.java
+++ Mocha04/src/main/java/mocha/ezen/com/attachment/AttachmentController.java
@@ -1,5 +1,45 @@
 package mocha.ezen.com.attachment;
 
-public class AttachmentController {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.OutputStream;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import mocha.ezen.com.approval.ApprovalRepository;
+
+public class AttachmentController 
+{
+	private static final String uploadPath = "D:\\jhjeong\\workspace\\Mocha\\upload";
+	
+	//첨부파일 다운로드
+		@RequestMapping(value = "/Download")
+		public void Download(@RequestParam(required = true) String no,
+				HttpServletResponse response) throws Exception
+		{
+			AttachmentDTO dto = ApprovalRepository.Read(no, true);
+			
+			File file = new File(uploadPath, dto.getPname());
+			
+			// 파일명 인코딩
+			//String filename = vo.getFname();
+			String filename = new String(dto.getFname().getBytes("UTF-8"),
+					"ISO-8859-1");
+					
+			// file 다운로드 설정
+			response.setContentType("application/download");
+			response.setContentLength((int)file.length());
+			response.setHeader("Content-Disposition", "attatchment;filename=\"" + filename + "\"");
+			
+			// 다운로드 시 저장되는 이름은 Response Header의 "Content-Disposition"에 명시
+			OutputStream os = response.getOutputStream();
+			
+			FileInputStream fis = new FileInputStream(file);
+			FileCopyUtils.copy(fis, os);
+			
+		}
 }
